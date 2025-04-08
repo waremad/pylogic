935aeaeb75394aa2a47accf3196e4f127af5f125
waremad <mycarline5@gmail.com>
2025-04-08 22:43:24 +0900

    8bit減算器まで

--- pylogic.py
+++ pylogic.py
@@ -1,9 +1,9 @@
 
 def NOT(a):
-    return -a+1
+    return str(-int(a)+1)
 
 def AND(a,b):
-    return a*b
+    return str(int(a)*int(b))
 
 def OR(a,b):
     return NOT(AND(NOT(a),NOT(b)))
@@ -18,6 +18,22 @@ def FullAdd(a,b,x=0):#全加算器
     c,s = HalfAdd(a,b)
     c,s2 = HalfAdd(c,x)
     return c,OR(s,s2)
+
+def point(self,i,n):#selfのi番目をnにする
+    self = list(self)
+    self[i] = str(n)
+    return "".join(self)
+
+def bitnum(n):#bitをそろえる
+    bit = 8
+    if bit < len(n):
+        n = n[::-1]
+        n = n[:bit]
+        return n[::-1]
+
+    for i in range(bit - len(n)):
+        n = "0" + n
+    return n
 
 def EightBitAdd(a,b):#8bit加算器
     bit = 8
@@ -37,6 +53,16 @@ def EightBitSub(a,b):#8bit減算器
     bit = 8
     
     for i in range(bit):
-        #b[i] = str(NOT(int(b[i])))
+        b = point(b,i,NOT(b[i]))
+    b = EightBitAdd(bitnum("1"),b)
     
-    return b
+    return EightBitAdd(a,b)
+
+def MoreThanA(a,b):#aがbより大きいか
+    pass
+
+def MoreEqualB(a,b):#aがb以上か
+    pass
+
+def EqualB(a,b):#aとbが等しいか
+    pass
--- test_pylogic.py
+++ test_pylogic.py
@@ -1,43 +1,43 @@
 from pylogic import * 
 
 def test_NOT():
-    assert NOT(0) == 1
-    assert NOT(1) == 0
+    assert NOT("0") == "1"
+    assert NOT("1") == "0"
 
 def test_AND():
-    assert AND(0,0) == 0
-    assert AND(1,0) == 0
-    assert AND(0,1) == 0
-    assert AND(1,1) == 1
+    assert AND("0","0") == "0"
+    assert AND("1","0") == "0"
+    assert AND("0","1") == "0"
+    assert AND("1","1") == "1"
 
 def test_OR():
-    assert OR(0,0) == 0
-    assert OR(1,0) == 1
-    assert OR(0,1) == 1
-    assert OR(1,1) == 1
+    assert OR("0","0") == "0"
+    assert OR("1","0") == "1"
+    assert OR("0","1") == "1"
+    assert OR("1","1") == "1"
 
 def test_XOR():
-    assert XOR(0,0) == 0
-    assert XOR(1,0) == 1
-    assert XOR(0,1) == 1
-    assert XOR(1,1) == 0
+    assert XOR("0","0") == "0"
+    assert XOR("1","0") == "1"
+    assert XOR("0","1") == "1"
+    assert XOR("1","1") == "0"
 
 def test_HalfAdd():
-    assert HalfAdd(0,0) == (0,0)
-    assert HalfAdd(1,0) == (1,0)
-    assert HalfAdd(0,1) == (1,0)
-    assert HalfAdd(1,1) == (0,1)
+    assert HalfAdd("0","0") == ("0","0")
+    assert HalfAdd("1","0") == ("1","0")
+    assert HalfAdd("0","1") == ("1","0")
+    assert HalfAdd("1","1") == ("0","1")
 
 def test_FullAdd():
-    assert FullAdd(0,0,0) == (0,0)
-    assert FullAdd(0,0,1) == (1,0)
-    assert FullAdd(0,1,0) == (1,0)
-    assert FullAdd(0,1,1) == (0,1)
+    assert FullAdd("0","0","0") == ("0","0")
+    assert FullAdd("0","0","1") == ("1","0")
+    assert FullAdd("0","1","0") == ("1","0")
+    assert FullAdd("0","1","1") == ("0","1")
 
-    assert FullAdd(1,0,0) == (1,0)
-    assert FullAdd(1,0,1) == (0,1)
-    assert FullAdd(1,1,0) == (0,1)
-    assert FullAdd(1,1,1) == (1,1)
+    assert FullAdd("1","0","0") == ("1","0")
+    assert FullAdd("1","0","1") == ("0","1")
+    assert FullAdd("1","1","0") == ("0","1")
+    assert FullAdd("1","1","1") == ("1","1")
 
 def TenToTwoEight(n):#10進数から2進数8bitに変換
     bit = 8
@@ -84,6 +84,26 @@ def test_TwoToTenEight():
     assert TwoToTenEight("11110001")  ==  -15
     assert TwoToTenEight("10000001")  == -127
     assert TwoToTenEight("10000000")  == -128
+
+def test_point():
+    assert point("00000000",0,1) == "10000000"
+    assert point("00000000",1,1) == "01000000"
+    assert point("00000000",2,1) == "00100000"
+    assert point("00000000",3,1) == "00010000"
+    assert point("00000000",4,1) == "00001000"
+    assert point("00000000",5,1) == "00000100"
+    assert point("00000000",6,1) == "00000010"
+    assert point("00000000",7,1) == "00000001"
+    assert point("11111111",5,1) == "11111111"
+
+def test_bitnum():
+    assert bitnum("00000000") == "00000000"
+    assert bitnum("1") == "00000001"
+    assert bitnum("01") == "00000001"
+    assert bitnum("0000001") == "00000001"
+    assert bitnum("000000001") == "00000001"
+    assert bitnum("00000000001") == "00000001"
+    assert bitnum("010011000111") == "11000111"
 
 def test_EightBitAdd():
     num = [1,2,3,5,7,11,99,28,6,57]
